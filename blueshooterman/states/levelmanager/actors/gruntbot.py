59bcc573bf3782b41b42c7a2d31903b17b040578
Kennedy Richard <kennedy@kennedyrichard.com>
2022-10-29 16:15:02 -0300

    changed: grunt bot now twinkles white when hit

--- blueshooterman/states/levelmanager/actors/gruntbot.py
+++ blueshooterman/states/levelmanager/actors/gruntbot.py
@@ -2,11 +2,17 @@
 from functools import partial
 
 
+### third-party import
+from pygame.time import get_ticks as get_msecs
+
+
 ### local imports
 
 from ....config import ACTORS, FRONT_PROPS, append_task
 
 from ....ani2d.player import AnimationPlayer2D
+
+from ....ourstdlibs.behaviour import do_nothing
 
 from ..frontprops.defaultexplosion import DefaultExplosion
 
@@ -25,7 +31,15 @@ class GruntBot:
             )
         )
 
-    def update(self): pass
+        self.last_damage = get_msecs()
+        self.update = do_nothing
+
+    def check_damage_whitening(self):
+
+        if get_msecs() - self.last_damage > 70:
+
+            self.aniplayer.blend('-white')
+            self.update = do_nothing
 
     def draw(self):
         self.aniplayer.draw()
@@ -40,3 +54,7 @@ class GruntBot:
 
             FRONT_PROPS.add(DefaultExplosion('center', center))
             append_task(partial(ACTORS.remove, self,))
+
+        else:
+            self.aniplayer.blend('+white')
+            self.update = self.check_damage_whitening
